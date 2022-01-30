9ec0a19e91c042133b6f7ef914d5d19008f327f8
Vihan Perera <47180113+hanzperera99@users.noreply.github.com>
2022-01-30 17:49:17 +0530

    Working on video array

--- assets/js/checking.js
+++ assets/js/checking.js
@@ -1,9 +1,8 @@
 var linkArray = ["aaa", "bbb", "ccc"];
 console.log(linkArray[0]);
 
-for (let ind = 0; ind < array.length; ind++) {
-    const element = array[ind];
-    
+for (let i = 0; i < linkArray.length; i++) {
+    console.log(linkArray[i])   
 }
 
 document.getElementById("links").innerHTML = "Links added";
